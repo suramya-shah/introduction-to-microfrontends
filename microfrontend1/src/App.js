8c60dc179b40a994a7d6ddf4be9a8df8853a5940
Suramya Shah <21042045+suramya-shah@users.noreply.github.com>
2023-06-26 09:48:21 +0530

    Create App.js

---
+++ microfrontend1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from './components/Button';
+import Card from './components/Card';
+
+function App() {
+  return (
+    <div>
+      <h1>Microfrontend 1</h1>
+      <Button />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
