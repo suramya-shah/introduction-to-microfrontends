7ae0c801f0c73ab26de33adbdf33729f070a83d4
Suramya Shah <21042045+suramya-shah@users.noreply.github.com>
2023-07-24 00:07:59 +0530

    Update webpack.config.js

--- microfrontend1/src/webpack.config.js
+++ microfrontend1/src/webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: './index.js',
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'microfrontend1', 'dist'),
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
