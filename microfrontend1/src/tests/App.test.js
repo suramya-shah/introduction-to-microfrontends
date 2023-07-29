906dadb383ec2189e3960685c995607cdb20b187
Suramya Shah <21042045+suramya-shah@users.noreply.github.com>
2023-07-29 06:15:45 -0700

    Update App.test.js

--- microfrontend1/src/tests/App.test.js
+++ microfrontend1/src/tests/App.test.js
@@ -1,12 +1,15 @@
+// src/App.test.js
 
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'; // Import the testing library with the extend-expect
+
 import App from '../App';
 
 test('renders Microfrontend 1 heading', () => {
   render(<App />);
-  const headingElement = screen.getByText('Microfrontend 1');
-  expect(headingElement).toBeInTheDocument();
+  const headingElement = screen.queryByText('Microfrontend 1'); // Use queryByText
+  expect(headingElement).toBeInTheDocument(); // Use toBeInTheDocument
 });
 
 test('renders Button component', () => {
@@ -17,6 +20,6 @@ test('renders Button component', () => {
 
 test('renders Card component', () => {
   render(<App />);
-  const cardElement = screen.getByText('Card Title');
-  expect(cardElement).toBeInTheDocument();
+  const cardElement = screen.queryByText('Card Title'); // Use queryByText
+  expect(cardElement).toBeInTheDocument(); // Use toBeInTheDocument
 });
