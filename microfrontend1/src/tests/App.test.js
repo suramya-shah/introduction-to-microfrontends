4bed4051423536a68064874dcfe7d5133bb23a8d
Suramya Shah <21042045+suramya-shah@users.noreply.github.com>
2023-07-29 06:04:40 -0700

    Update App.test.js

--- microfrontend1/src/tests/App.test.js
+++ microfrontend1/src/tests/App.test.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
 test('renders Microfrontend 1 heading', () => {
   render(<App />);
